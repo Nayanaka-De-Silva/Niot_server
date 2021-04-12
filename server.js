52a48887f97fde3d08fc64d1abc25072999df47a
Nayanaka De Silva <k.nayanaka.desilva@gmail.com>
2021-04-12 14:17:22 +0800

    Changed achitecture to run using WebSocket technology
    The project itself isn't going much further due to the complicated limitations of using a CRUD server.
    Instead, the more ideal option is to use WebSockets so that the server can utilize the web socket and can send and receive data at will.

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
+// Import our needed packages
 const express = require('express');
-const port = process.env.PORT || 3000;
-const app = express();  // Create Express App
+const http = require('http');
+const WebSocket = require('ws');
+const app = express();
 
-app.get('/', (req, res) => {
-    res.send(`Hello world!`);
-})
+// Define the port that's going to be used
+const port = process.env.PORT || 4000;
 
-app.get('/test', (req, res) => {
-    res.send(`Test`);
-})
+// Initialize a simple http server
+const server = http.createServer(app);
 
-app.listen(port, () => {
-    console.log(`Server listening at port ${port}`);
+// Initialize the WebSocket server
+const wss = new WebSocket.Server({ server });
+
+// Check for incoming connections
+wss.on('connection', (ws)=>{
+    // When a connection has been made, this function will run
+    ws.on('message', (message)=>{
+        // This function will run when the server receives a message
+        console.log(`Received message ${message}`);
+        ws.send(`Server received: ${message}`);
+    });
+    ws.send('Welcome to NIoT server');
 });
+
+server.listen(port, ()=>{
+    console.log(`Server started on port ${port}`);
+})
