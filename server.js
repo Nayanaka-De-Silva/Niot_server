c7f0cc5edffaf6d73e74a85226043ceb806e273b
Krystler <k.nayanaka.desilva@gmail.com>
2022-01-08 01:34:11 +0800

    Init MQTT Functionality
    The project is now moving away from yet another communication protocol.
    The previous implementations worked well with the server, but didn't
    communicate as well with the other devices. This communication protocol
    is lightweight and robust. Hence why the server will now be using MQTT
    to communicate with the rest of the NioT network.

--- server.js
+++ server.js
@@ -1,30 +1,20 @@
-// Import our needed packages
-const express = require('express');
-const http = require('http');
-const WebSocket = require('ws');
+require('dotenv').config();
+const express = require("express");
+const { createServer } = require("http");
+const port = process.env.PORT || 3000;
+const mqtt = require('mqtt');
+const client = mqtt.connect('mqtt://192.168.100.85:1883');
+
 const app = express();
 
-// Define the port that's going to be used
-const port = process.env.PORT || 4000;
-
-// Initialize a simple http server
-const server = http.createServer(app);
-
-// Initialize the WebSocket server
-const wss = new WebSocket.Server({ server });
-
-// Check for incoming connections
-wss.on('connection', (ws)=>{
-    // When a connection has been made, this function will run
-    ws.on('message', (message)=>{
-        // This function will run when the server receives a message
-        console.log(`Received message ${message}`);
-        ws.send(`Server received: ${message}`);
-    });
-    ws.send('Welcome to NIoT server');
-    console.log('New client connected.'); 
-});
-
-server.listen(port, ()=>{
-    console.log(`Server started on port ${port}`);
+client.on('connect', () => {
+    client.subscribe('test', (err) => {
+        if(!err) console.log('Subscribed to topic: test');
+    })
 })
+
+client.on('message', (topic, message) => {
+    console.log(message.toString());
+})
+
+app.listen(port);
